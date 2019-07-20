5528efbda5c3dd1f88f6c2c92fbbd3fb0da0f58d
spikeburton <ralphburton3@gmail.com>
2019-07-20 15:39:15 -0400

    return relavent data

--- index.js
+++ index.js
@@ -7,11 +7,24 @@ exports.handler = async () => {
       `https://medium.com/@${username}/latest?format=json`
     );
 
-    const response = {
+    const result = [];
+    const posts = JSON.parse(res.data.replace('])}while(1);</x>', '')).payload
+      .references.Post;
+    for (let i in posts) {
+      let post = {};
+
+      post.id = posts[i].id;
+      post.title = posts[i].title;
+      post.slug = posts[i].uniqueSlug;
+      post.firstPublishedAt = posts[i].firstPublishedAt;
+
+      result.push(post);
+    }
+
+    return {
       statusCode: 200,
-      body: JSON.parse(res.data.replace('])}while(1);</x>', ''))
+      body: result
     };
-    return response;
   } catch (e) {
     return {
       statusCode: 400,
